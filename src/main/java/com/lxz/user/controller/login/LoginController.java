570ac9bce026fd7332e5a82737aa88cd306f5e47
hujixiang <hujixiang@nl-ds.cn>
2025-08-15 18:05:08 +0800

    初始化

--- src/main/java/com/lxz/user/controller/login/LoginController.java
+++ src/main/java/com/lxz/user/controller/login/LoginController.java
@@ -1,7 +1,9 @@
 package com.lxz.user.controller.login;
 
+import com.lxz.user.service.TestTableUserService;
 import com.lxz.user.vo.login.LoginDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 @Validated
 public class LoginController {
+    @Autowired
+    private TestTableUserService test;
 
     @PostMapping("/login")
     public void login(@RequestBody @Validated LoginDto loginDto) {
+        test.getById(1);
         log.info(loginDto.getPassword());
         System.out.println("xxxxxxxxxxx");
     }
